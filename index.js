1537e75d8b6fd73b57ca264c608d04dbab9ec0e4
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-28 11:49:54 -0500

    Feat: Compile typescript file

---
+++ index.js
@@ -0,0 +1,3 @@
+//Setting up TS files
+var age = 30; //Cannot change to a string in future if assigned an int
+console.log(age);
--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 //Setting up TS files
+let age = 30 //Cannot change to a string in future if assigned an int
+
+console.log(age)
