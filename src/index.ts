4eae99ffaeb3323db7164d60a41cf6d3557f8130
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-28 17:16:13 -0500

    Feat: Type inferance with arrays in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-//Arrays
+//******* Arrays ***********/
+
 let names: string[] = ['Matt', 'Marco', 'Jennifer'] //array of ONLY strings
 let numbers : number[] = [1, 2, 3] // Array of only Numbers
 let many: any[] = [1, "Name"]// Any values
@@ -9,6 +10,18 @@ names.push('Ben');
 numbers.push(2)
 //numbers.push('String') // Won't work obviously
 
-//Type Inferance with object literals
+//***** Type Inferance with arrays ****//
 
-//Object Literals
+let fruits = ['Apples', 'Oranges', 'Bananas'] // Type inferance makes this the same as let fruits: string[] = [];
+
+//fruits.push(1); //Type inferance will only allow strings to be pushed
+fruits.push('Mangos')
+
+const f = fruits[3] //shows type will be a string
+
+let things = [1, true, 'hello'];
+const t = things[0]; // It shows it can either be a string, number of boolean
+
+//****************** Object Literals ****************//
+
+//***** Type Inferance with object literals *********//
