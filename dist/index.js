d2d670e7b4f0a4e912bcbbd7d1ddce56048098e2
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-30 16:57:14 -0500

    Feat: The any type in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+//********** any type **************/
+let myAge;
+myAge = 30;
+myAge = false;
+myAge = "55";
+//Won't cause any errors because it accepts "any" value type, but you should try to use this as less as possible
+//inerfence any type
+let title;
+title = 25;
+title = {
+    hello: 'hi',
+};
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,19 @@
-//************ Functions *******/
+//********** any type **************/
 
-// Javascript Version
-// function addTwoNumbers(a, b) {
-//     return a + b;
-// }
+let myAge: any
 
-//Give the parameters a type - you can also give the return value a type
-function addTwoNumbers(a: number, b: number): number{
-    return a + b;
+myAge = 30
+myAge = false
+myAge = "55"
+
+//Won't cause any errors because it accepts "any" value type, but you should try to use this as less as possible
+
+//inerfence any type
+let title
+
+title = 25
+title = {
+    hello: 'hi',
 }
 
 
-//Arrow function version
-const subtractTwoNumbers = (a: number, b: number): number => {
-    return a - b;
-}
-
-//addTwoNumbers('a', '2'); Won't work
-addTwoNumbers(1, 2)
-
-subtractTwoNumbers(2, 1)
-
-//void type because we are not returning anything
-function addAllNumbers(items: number[]): void {
-    const total = items.reduce((a,c) => a + c, 0); //reduce can be used because type if an array
-    console.log(total);
-}
-
-addAllNumbers([1,5, 6, 7, 11, 34, 3, 11]);
-
-// ******** Return Type Inference ******///////
-
-function formatGreeting(name: string, greeting: string) {
-    return `${greeting}, ${name}`
-}
-
-//No return type addeed but it infers the return type will be string
-const result = formatGreeting('Matthew', 'Hello');
