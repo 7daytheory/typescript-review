d62d41642263031c628b70aef744a79d0a9e5cfe
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-30 17:13:31 -0500

    Feat: Tuples and some examples

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
 /******* Tuples ******/
 let person = ['Matt', 32, false];
+/******** Examples ****/
+let hsla; // hsla Common color coding type
+hsla = [200, '100%', '50%', 1];
+let xy;
+xy = [94.7, 20.1];
+function useCoords() {
+    //get coords
+    const lat = 100;
+    const long = 50;
+    return [lat, long];
+}
+const [lat, long] = useCoords();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
 /******* Tuples ******/
 let person: [string, number, boolean] = ['Matt', 32, false]
+
+
+/******** Examples ****/
+
+let hsla: [number, string, string, number] // hsla Common color coding type
+hsla = [200, '100%', '50%', 1]
+
+let xy: [number, number]
+xy = [94.7, 20.1]
+
+function useCoords(): [number, number] {
+    //get coords
+
+    const lat = 100
+    const long = 50
+
+    return [lat, long]
+}
+
+const [lat, long] = useCoords()
