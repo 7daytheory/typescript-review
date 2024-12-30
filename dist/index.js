3312f256148dfa72a29812b196754e6807d6488a
Matthew Lowe <matthewjlowe11@gmail.com>
2024-12-30 17:34:46 -0500

    Feat: Setting up and using Interfaces

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,12 @@
 /************
 / Interfaces
  ***********/
+const authorOne = {
+    name: 'Matthew',
+    avatar: '../url/link.png'
+};
+//Useful when re-using for many cases
+const authorTwo = {
+    name: 'John',
+    avatar: '../url/link.png'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 /************
 / Interfaces
  ***********/
+
+//Several objects that will all be the same (users for example)
+
+//Pascal case for interfaces generally
+interface Author {
+    name: string,
+    avatar: string,
+}
+
+const authorOne: Author = {
+    name: 'Matthew',
+    avatar: '../url/link.png'
+}
+
+//Useful when re-using for many cases
+const authorTwo: Author = {
+    name: 'John',
+    avatar: '../url/link.png'
+}
